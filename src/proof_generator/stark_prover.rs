231d06ce8d8af7fdde17bb9b281fe8950cd5c714
codebestia <codebestia@gmail.com>
2025-03-30 21:57:16 +0100

    chore: add documentation to functions

--- src/proof_generator/stark_prover.rs
+++ src/proof_generator/stark_prover.rs
@@ -18,6 +18,11 @@ pub struct StarkProver {
 }
 
 impl StarkProver {
+    /// Create a new StarkProver instance
+    /// @params db_pool reference to a PgPool instance (PgPool)
+    /// @params merkle_tree_data reference to a pre-validated merkle tree data (MerkleConfig)
+    /// @params commitment_hash the commitment hash of the transaction (string)
+    /// returns Self The instance of StarkProver
     pub fn new(db_pool: PgPool, merkle_tree_data: MerkleConfig, commitment_hash: &str) -> Self {
         Self {
             db_pool,
@@ -25,25 +30,41 @@ impl StarkProver {
             commitment_hash: commitment_hash.to_owned(),
         }
     }
-
+    /// Generate the STARK Proof
+    /// return String The stark proof
     pub fn generate_proof(&self) -> String {
-        
+        // Proof logic here
+        "proof".to_owned()
     }
 
-    async  fn save_proof_to_db(&self, merkle_tree_root: &str, commitment_hash: &str) -> Result<(), ProofGenerationError>{
+    /// Saves the STARK Proof and the metadata to DB
+    /// @params merkle_tree_root The root of the merkle tree data
+    /// @params proof The generated proof (string)
+    /// @params commitment_hash The pre-validated commitment hash of the transaction
+    /// returns Result<(), ProofGenerationError>
+    async  fn save_proof_to_db(&self, merkle_tree_root: &str, proof: &str, commitment_hash: &str) -> Result<(), ProofGenerationError>{
         sqlx::query!(
             r#"
-            INSERT INTO stark_proofs (merkle_tree_root, commitment_has, updated_at)
-            VALUES ($1, $2, NOW())
+            INSERT INTO stark_proofs (merkle_tree_root, commitment_hash, proof, updated_at)
+            VALUES ($1, $2, $3, NOW())
             "#,
             merkle_tree_root,
             commitment_hash,
+            proof
         )
         .execute(self.db_pool)
         .await
         .map_err(|e| ProofGenerationError::DatabaseError(e))?;
 
         Ok(())
+    }
+    
+    /// Runs the StarkProver application
+    /// generates the proof and save it to db
+    async fn run(&self) {
+        let proof = self.generate_proof();
+        // save the data to database
+        self.save_proof_to_db(self.merkle_tree_root, &proof, &self.commitment_hash).await?;
     }
 
 }
